2c2b6d719ea6916ed67cf800f35d331a0f2a3989
Ghadeer <202204965@stu.uob.edu.bh>
2025-11-20 16:10:42 +0300

    Select the assignment list section in list.js

--- src/assignments/list.js
+++ src/assignments/list.js
@@ -13,6 +13,7 @@
 
 // --- Element Selections ---
 // TODO: Select the section for the assignment list ('#assignment-list-section').
+const listSection = document.getElementById('assignment-list-section');
 
 // --- Functions ---
 
